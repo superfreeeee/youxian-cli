36d22bc92ae710e627fd2622d78bfddc5ab71062
superfree <superfreeeee@gmail.com>
2021-10-28 17:59:41 +0800

    update template

--- templates/app-fe/src/App.tsx
+++ templates/app-fe/src/App.tsx
@@ -1,13 +1,30 @@
-import styles from './index.module.scss';
-
 import React, { FC } from 'react';
 import classNames from 'classnames';
+import styled, { createGlobalStyle } from 'styled-components';
+
+const AppRoot = styled.div``;
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+  }
+
+  body {
+    min-width: 100vw;
+    min-height: 100vh;
+    margin: 0;
+  }
+`;
 
 const App: FC = () => {
   return (
-    <div className={classNames(styles.appRoot)}>
+    <AppRoot>
+      <GlobalStyle />
       <h1>Application Front-end</h1>
-    </div>
+    </AppRoot>
   );
 };
 
